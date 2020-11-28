286c7a23cdf569d65cbc55d8ca217d8355243279
MOKSHdaBEST <74857781+MOKSHdaBEST@users.noreply.github.com>
2020-11-28 15:48:14 +0530

    first calculatore update

---
+++ calculater.js
@@ -0,0 +1,23 @@
+let startbtn = document.querySelectorAll("button");//list of all buttons in html
+
+for(var button of startbtn){
+button.addEventListener("click",displayNumber);
+}
+function displayNumber(event){
+    var ids = event.target.id;
+    
+    if(ids == 'equal'){
+        var currentDisplay = document.getElementById('display').innerHTML;
+        var result = eval(currentDisplay);
+        document.getElementById('display').innerHTML = ""+result;
+    }
+    
+    else if(ids == 'clear'){
+        document.getElementById('display').innerHTML = "";
+    }else{
+    var btid= event.target.innerHTML;
+    var currentDisplay =document.getElementById('display').innerHTML;
+    document.getElementById('display').innerHTML = currentDisplay + btid;
+}
+
+}
